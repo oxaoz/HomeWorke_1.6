483d61ac3de81deab1a3501404a90b70ef0b4550
oxaoz <ogozer@ya.ru>
2022-10-08 16:35:09 +0700

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,26 @@ public class Main {
         for (int i = 10; i > -10; i--) {
             System.out.println("Все числа от 10 до -10 = " + i);
         }
+
+        // Задание 2, Задача 1.
+        System.out.println("Задание 2, Задача 1.");
+
+        for (int i = 1904; i < 2096; i = i + 4){
+            System.out.println(i + " год является високосным");
+        }
+
+        // Задание 2, Задача 2.
+        System.out.println("Задание 2, Задача 2.");
+
+        for (int i = 7; i < 100; i = i + 7 ){
+            System.out.println(i);
+        }
+
+        // Задание 2, Задача 3.
+        System.out.println("Задание 2, Задача 3.");
+
+        for (int i = 1; i < 515; i = i * 2){
+            System.out.println(i);
+        }
     }
 }
